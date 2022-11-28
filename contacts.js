ac4d00e3afbd56ecd62bd5ee17a576208cdfa9b6
AleksandrTikka <alexandertikka@gmail.com>
2022-11-28 18:24:17 +0200

    fix index

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
-const { v4: uuidv4 } = require("uuid");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const contactsPath = path.resolve("./db/contacts.json");
 
 const listContacts = async () => {
@@ -17,11 +17,13 @@ const listContacts = async () => {
 const getContactById = async (contactId) => {
   try {
     const contacts = await listContacts();
-    const contactById = contacts.find((contact) => contact.id === contactId);
+    const idString = contactId.toString();
+    const contactById = contacts.find((contact) => contact.id === idString);
     if (!contactById) {
       console.log(`There is no contact with ID: ${contactId}`);
       return;
     }
+    console.log(contactById);
     return contactById;
   } catch (error) {
     console.log(error.message);
@@ -41,8 +43,9 @@ const removeContact = async (contactId) => {
       (contact) => contact.id !== contactId
     );
     const newData = JSON.stringify(filteredContacts);
-    await fs.writeFile(contactsPath, newData);
+    fs.writeFile(contactsPath, newData);
     console.log(`Contact with ID: ${contactId} has been removed successfully!`);
+    console.log(`Removed contact: ${selectedContact}`);
   } catch (error) {
     console.log(error.message);
   }
@@ -53,7 +56,8 @@ const addContact = async (name, email, phone) => {
     const contacts = await listContacts();
     const newContact = { id: uuidv4(), name, email, phone };
     const newData = JSON.stringify([newContact, ...contacts]);
-    await fs.writeFile(contactsPath, newData, "utf-8");
+    await fs.writeFile(contactsPath, newData);
+    console.log(newData);
   } catch (error) {
     console.log(error.message);
   }
--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const {
 } = require("./contacts");
 
 listContacts();
-console.log(listContacts());
+getContactById(3);
+removeContact(1);
+addContact("Aleksandr", "aleksandr@mail.com", 0666665544);
